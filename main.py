d201cd09fee8025cc66097ef6286f9bd640acf3c
WhaeaTKV <kapa-viviant@wsc.school.nz>
2026-07-01 13:50:07 +1200

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+print("Welcome to my Quiz about _!")
+input("Are you ready to begin?")
+
+print("Question 1: What is the capital of New Zealand?")
+input("Is the answer: A: Auckland B: Wellington or C: Christchurch?")
